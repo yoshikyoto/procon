627ea2ae410844cceb11f0850eabf9b805ec98dc
yoshikyoto <admin@h96.26.238.10.30790.vlan.kuins.net>
2014-10-27 15:26:19 +0900

    initial commit

---
+++ aoj/B.h
@@ -0,0 +1,14 @@
+//
+//  B.h
+//  aoj
+//
+//  Created by sakamoto on 2014/07/05.
+//  Copyright (c) 2014年 sakamoto. All rights reserved.
+//
+
+#ifndef __aoj__B__
+#define __aoj__B__
+
+#include <iostream>
+
+#endif /* defined(__aoj__B__) */
---
+++ aoj/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <set>
+#include <cstdio>
+using namespace std;
+
+#define FOR(i, n) for(int i = 0; i < n; i++)
+
+
+int main(int argc, const char * argv[]){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+}
+
+
